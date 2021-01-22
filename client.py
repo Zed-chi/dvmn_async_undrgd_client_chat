df3ccd32bf1761f060c8f1b9c2f9f4b9e45a9fdc
zed-chi <zed-chi@mail.ru>
2021-01-22 15:19:01 +0300

    added logging feature

--- client.py
+++ client.py
@@ -1,7 +1,12 @@
 import asyncio
+import aiofiles
+from datetime import datetime
+#import logging
+
 
 HOST = "minechat.dvmn.org"
 PORT = 5000
+LOG_NAME = "history.log"
 
 async def tcp_echo_client():
     reader, _ = await asyncio.open_connection(
@@ -9,11 +14,17 @@ async def tcp_echo_client():
 
     while True:
         data = await reader.readline()
-        print(f'{data.decode()}')
+        message = data.decode("utf-8")
+        now = datetime.now().strftime("[%d.%m.%y %H:%M]")
+        async with aiofiles.open(LOG_NAME, mode='a', encoding="utf-8") as f:
+            await f.write(f"{now} {message}")
+        print(f"{now} {message.strip()}")
 
 if __name__ == "__main__":
     try:
         asyncio.run(tcp_echo_client())
-    except:
+    except KeyboardInterrupt:
         print("Client disconnected")
+    except Exception as e:
+        print(e)
 
