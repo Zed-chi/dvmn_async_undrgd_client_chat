67cb082e2257775a444b55481c6165b2e3b6f341
zed <zed@mail.ru>
2021-01-24 01:19:17 +0300

    lint fix

--- utils.py
+++ utils.py
@@ -9,8 +9,8 @@ def get_args():
         ]
     )
     p.add("--debug", required=True, help="debug")
-    p.add("--host", required=True, help="host address")    
-    p.add("--sender_port", required=True, help="port address")    
+    p.add("--host", required=True, help="host address")
+    p.add("--sender_port", required=True, help="port address")
     p.add("--token", help="token", required=False)
     p.add("--port", required=True, help="port of the host")
     p.add("--log_path", required=True, help="history log path")
