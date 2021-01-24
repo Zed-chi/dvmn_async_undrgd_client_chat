445a10f3ee28209d9d175d223b254fe28a32bcf0
zed <zed@mail.ru>
2021-01-24 23:11:15 +0300

    moved args to file

--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 import configargparse
-import json
 
 
 def get_args():
     p = configargparse.ArgParser(
         default_config_files=[
             "./config.txt",
-        ]
+        ],
     )
-    p.add("--debug", required=True, help="debug")
     p.add("--host", required=True, help="host address")
-    p.add("--sender_port", required=True, help="port address")
+    p.add("--sender_port", required=True, help="port of sender client")
     p.add("--token", help="token", required=False)
-    p.add("--port", required=True, help="port of the host")
-    p.add("--log_path", required=True, help="history log path")
+    p.add("--port", required=True, help="port of the viewer client")
+    p.add("--sender_log_path", required=False, help="sender log path")
+    p.add("--history_path", required=False, help="history log path")
+    p.add("--name", required=False, help="name for registration")
+
     return p.parse_args()
